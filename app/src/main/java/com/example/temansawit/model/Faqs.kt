9a77ac0e2e6cf9609a874ead228b09b3a6c15614
noorsaputri123 <c030320077@mahasiswa.poliban.ac.id>
2023-05-28 13:00:37 +0700

    completed page faq & page faq details

--- app/src/main/java/com/example/temansawit/model/Faqs.kt
+++ app/src/main/java/com/example/temansawit/model/Faqs.kt
@@ -4,16 +4,16 @@ package com.example.temansawit.model
 object Faqs {
     val FAQS = listOf(
         faqData(
-           1, "Bagaimana cara membuat akun", "Aku tidak tau"
+           1, "Apa itu TemanSawit", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
        ),
         faqData(
-            2, "Bagaimana cara membuat akun", "Aku tidak tau"
+            2, "Apa manfaat dari TemanSawit", "Aku tidak tau"
         ),
         faqData(
-            3, "Bagaimana cara membuat akun", "Aku tidak tau"
+            3, "Bagaimana cara menambahkan transaksi", "Aku tidak tau"
         ),
         faqData(
-            4, "Bagaimana cara membuat akun", "Aku tidak tau"
+            4, "Bagaimana cara mengganti password", "Aku tidak tau"
         ),
     )
 }
--- app/src/main/java/com/example/temansawit/ui/screen/faq/FaqScreen.kt
+++ app/src/main/java/com/example/temansawit/ui/screen/faq/FaqScreen.kt
@@ -1,65 +1,89 @@
 package com.example.temansawit.ui.screen.faq
 
+import androidx.compose.ui.graphics.Color
+import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.example.temansawit.R
 import com.example.temansawit.model.Faqs.FAQS
+import com.example.temansawit.ui.theme.GreenPrimary
 
-
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun FaqScreen(
     modifier: Modifier = Modifier
 ) {
-    var openIndexs= remember {
-        mutableStateOf(-1)
-    }
+    val openIndexs = remember { mutableStateOf(-1) }
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "FAQ",textAlign = TextAlign.Center,modifier =Modifier.fillMaxWidth())},
+                backgroundColor = MaterialTheme.colors.primaryVariant
+            )
+        }
+    ) {
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            itemsIndexed(FAQS) {
-                index, item ->
-                Column (modifier = Modifier.fillMaxWidth() ) {
-                    ClickableText(text = AnnotatedString(item.question),
-                    onClick = {
-
-                        if (openIndexs.value == index) {
-                            openIndexs.value = -1
-
-                        } else {
-                            openIndexs.value = index
+            itemsIndexed(FAQS) { index, item ->
+                Card(modifier = Modifier.fillMaxWidth()) {
+                    Row(modifier = Modifier.padding(16.dp)) {
+                        Box(modifier = Modifier.weight(1f)) {
+                            ClickableText(
+                                text = AnnotatedString(item.question),
+                                onClick = {
+                                    openIndexs.value = if (openIndexs.value == index) -1 else index
+                                    Log.d("TESTES", openIndexs.value.toString())
+                                }
+                            )
                         }
-                        Log.d("TESTES", openIndexs.value.toString())
-
+                        Icon(
+                            painter = painterResource(R.drawable.ic_keyboard),
+                            contentDescription = "Question Icon",
+                            modifier = Modifier.size(24.dp)
+                        )
                     }
-                        ) //row+icon
-
-                    if (openIndexs.value == index) Text(item.answer)
-
                 }
 
+                if (openIndexs.value == index) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                        backgroundColor = GreenPrimary.copy(alpha = 0.2f),
+                        elevation = 0.dp
+                    ) {
+                        Row(modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)) {
+                            Box(modifier = Modifier.weight(1f)) {
+                                Text(item.answer)
+                            }
+                        }
+                    }
+                }
             }
-            
-//            items(faqs) { Faq ->
-//                ListItem(
-//                    modifier = Modifier.padding(8.dp),
-//                    text = { Text(text = item) }
-//                )
-//            }
         }
-
+    }
 }
